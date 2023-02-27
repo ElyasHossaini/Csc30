39f2dbb8f5b0dc04066514f69e06d864be4cae7a
105154006 <105154006@L815WD220007.edu.cbe.ab.ca>
2023-02-27 11:57:42 -0700

    kiju

--- CSE3010/src/Mastery/BreakAPlate.java
+++ CSE3010/src/Mastery/BreakAPlate.java
@@ -3,9 +3,12 @@ package Mastery;
 import java.awt.EventQueue;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.text.DecimalFormat;
+
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.Action;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 
@@ -55,20 +58,40 @@ public class BreakAPlate {
 		
 		JLabel Plate = new JLabel("");
 		Plate.setIcon(new ImageIcon(BreakAPlate.class.getResource("/images/plates.jpg")));
-		Plate.setBounds(82, 31, 270, 83);
+		Plate.setBounds(82, 31, 276, 83);
 		frame.getContentPane().add(Plate);
 		
 		JLabel Win = new JLabel("");
-		Win.setBounds(184, 159, 120, 14);
+		Win.setBounds(165, 159, 120, 14);
 		frame.getContentPane().add(Win);
 		
-		JLabel WinImage = new JLabel("New label");
-		WinImage.setBounds(154, 184, 150, 66);
+		JLabel WinImage = new JLabel("");
+		WinImage.setIcon(new ImageIcon(BreakAPlate.class.getResource("/images/placeholder.jpg")));
+		WinImage.setBounds(175, 184, 103, 66);
 		frame.getContentPane().add(WinImage);
 		
 		JButton Play = new JButton("Play");
 		Play.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				int number = (int)Math.random()*100000;
+				
+				if((number / 2) % 2 == 0)
+				{
+					Plate.setIcon(AllBroken);
+					WinImage.setIcon(Tiger);
+					Win.setText("You Win a Tiger!");
+				}
+				else
+				{
+					Plate.setIcon(TwoBroken);
+					WinImage.setIcon(Stickers);
+					Win.setText("You win a Sticker!");
+				}
+				
+				System.out.println(number);
+				
+			
+	
 			
 			}
 		});
