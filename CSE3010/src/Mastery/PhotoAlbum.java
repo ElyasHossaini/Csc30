096056fad5eadcac94d53d5064753c756ab17aae
105154006 <105154006@L815WD220007.edu.cbe.ab.ca>
2023-02-28 10:22:36 -0700

    gg

--- CSE3010/src/Mastery/PhotoAlbum.java
+++ CSE3010/src/Mastery/PhotoAlbum.java
@@ -3,6 +3,12 @@ package Mastery;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
 
 public class PhotoAlbum {
 
@@ -38,6 +44,27 @@ public class PhotoAlbum {
 		frame = new JFrame();
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Photo Album");
+		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		lblNewLabel.setBounds(10, 11, 139, 27);
+		frame.getContentPane().add(lblNewLabel);
+		
+		JLabel Photo = new JLabel("New label");
+		Photo.setBounds(40, 65, 200, 165);
+		frame.getContentPane().add(Photo);
+		
+		JButton Next = new JButton("Next");
+		Next.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				int num = 0;
+			
+	
+			
+			}
+		});
+		Next.setBounds(310, 134, 89, 23);
+		frame.getContentPane().add(Next);
 	}
-
 }
