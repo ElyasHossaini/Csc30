14e05045e5579daecede098a55f5835387681241
105154006 <105154006@L815WD220007.edu.cbe.ab.ca>
2023-03-02 10:13:20 -0700

    dfg

--- CSE3010/src/Mastery/BreakAPlate.java
+++ CSE3010/src/Mastery/BreakAPlate.java
@@ -3,8 +3,6 @@ package Mastery;
 import java.awt.EventQueue;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.text.DecimalFormat;
-
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.ImageIcon;
--- CSE3010/src/Mastery/PhotoAlbum.java
+++ CSE3010/src/Mastery/PhotoAlbum.java
@@ -4,11 +4,15 @@ import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+
+
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import java.awt.Color;
 
 public class PhotoAlbum {
 
@@ -35,36 +39,88 @@ public class PhotoAlbum {
 	 */
 	public PhotoAlbum() {
 		initialize();
+		
+		
 	}
 
 	/**
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
+		ImageIcon Brook = new ImageIcon("..\\CSE3010\\src\\images\\brook.png");
+		ImageIcon Nami = new ImageIcon("..\\CSE3010\\src\\images\\nami.jpg");
+		ImageIcon Luffy = new ImageIcon("..\\CSE3010\\src\\images\\luffy.png");
+		ImageIcon Jimbei = new ImageIcon("..\\CSE3010\\src\\images\\jinbei.png");
+		ImageIcon Usopp = new ImageIcon("..\\CSE3010\\src\\images\\usopp.jpg");
+		ImageIcon Robin = new ImageIcon("..\\CSE3010\\src\\images\\robin.jpg");
+		
 		frame = new JFrame();
+		frame.getContentPane().setBackground(Color.ORANGE);
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		
-		JLabel lblNewLabel = new JLabel("Photo Album");
+		JLabel lblNewLabel = new JLabel("Straw Hat Pirates Wanted Poster Photo Album");
 		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		lblNewLabel.setBounds(10, 11, 139, 27);
+		lblNewLabel.setBounds(10, 11, 350, 43);
 		frame.getContentPane().add(lblNewLabel);
 		
-		JLabel Photo = new JLabel("New label");
-		Photo.setBounds(40, 65, 200, 165);
+		JLabel Photo = new JLabel("");
+		Photo.setIcon(new ImageIcon(PhotoAlbum.class.getResource("/images/luffy.png")));
+		Photo.setBounds(67, 65, 160, 165);
 		frame.getContentPane().add(Photo);
-		
 		JButton Next = new JButton("Next");
+		Next.setBackground(Color.WHITE);
+		
+		JLabel NameL = new JLabel("Captain: Straw Hat Luffy");
+		NameL.setBounds(237, 65, 187, 37);
+		frame.getContentPane().add(NameL);
+		
 		Next.addActionListener(new ActionListener() {
+			int Next = 0;
 			public void actionPerformed(ActionEvent e) {
-				int num = 0;
+				
+				
+				Next++;
+				if(Next == 1)
+				{
+					Photo.setIcon(Robin);
+					NameL.setText("Archeologist: Nico Robin");
+				}
+				else if(Next == 2)
+				{
+					Photo.setIcon(Usopp);
+					NameL.setText("Sniper: God Usopp");
+				}
+				else if(Next == 3)
+				{
+					Photo.setIcon(Jimbei);
+					NameL.setText("Helmsman: Jinbei");
+				}
+				else if(Next == 4)
+				{
+					Photo.setIcon(Brook);
+					NameL.setText("Musician: Brook");
+				}
+				else if(Next == 5)
+				{
+					Photo.setIcon(Nami);
+					NameL.setText("Navigator: Nami");
+				}
+				else if(Next > 5)
+				{
+					Photo.setIcon(Luffy);
+					NameL.setText("Captain: Straw Hat Luffy");
+					Next = 0;
+				}
 			
-	
-			
+				
 			}
 		});
 		Next.setBounds(310, 134, 89, 23);
 		frame.getContentPane().add(Next);
+		
+		
+		
 	}
 }
