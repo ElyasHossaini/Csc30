d7acaebc3c74a117753c0541e9194134fdcf953f
105154006 <105154006@L815WD220007.edu.cbe.ab.ca>
2023-04-20 09:19:07 -0600

    9:19am

---
+++ CSE3130/src/Skillbuilding/MusicP1.java
@@ -0,0 +1,10 @@
+package Skillbuilding;
+
+public class MusicP1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ CSE3130/src/Skillbuilding/RectangleP4.java
@@ -0,0 +1,10 @@
+package Skillbuilding;
+
+public class RectangleP4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
